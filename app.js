5986148ddb59e20b5ba96feecab0bd19c5ecc2cb
OscarAlonso10 <48787652+OscarAlonso10@users.noreply.github.com>
2020-06-10 18:33:59 +0200

    login

---
+++ app.js
@@ -0,0 +1,41 @@
+var bodyParser = require('body-parser')
+let express = require('express');
+var app = express()
+var LocalStorage = require('node-localstorage').LocalStorage;
+localStorage = new LocalStorage('./scratch');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+
+var users = {
+  'prueba1':'prueba1',
+  'prueba2':'prueba2',
+}
+
+app.get('/login', function (req, res){
+  res.render('loginform');
+});
+
+app.post('/login', function (req, res){
+  var user = "";
+  var inputUser = req.body.username;
+  var inputPass = req.body.password;
+  if (localStorage.getItem(inputUser) == inputPass) {
+        user = inputUser;
+  }
+  res.render('loginformPost', {user});
+});
+
+app.get('/register', function (req, res){
+  res.render('registerform');
+});
+
+app.post('/register', function (req, res){
+  var inputUser = req.body.username;
+  var inputPass = req.body.password;
+  localStorage.setItem(inputUser, inputPass);
+  res.render('registerformPost');
+});
+
+app.listen(3000, function(){
+  console.log('Exameple app listening on port 3000');
+});
